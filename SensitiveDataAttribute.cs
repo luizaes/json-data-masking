ec816ddb0ab22e2ca6df0b98355088c914697ec5
Luiza Engler Stadelhofer <luiza.engler@gmail.com>
2022-07-05 18:39:47 -0300

    feat: add attribute to indicate sensitive data

---
+++ SensitiveDataAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonDataMasking
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class SensitiveDataAttribute : Attribute
+    {
+        public bool PreserveLength;
+
+        public int ShowFirst;
+
+        public int ShowLast;
+
+        public string? Text;
+
+        public string Mask;
+
+        public SensitiveDataAttribute(bool preserveLength = true, int showFirst = 0, int showLast = 0,
+            string? text = null, string? mask = null)
+        {
+            PreserveLength = preserveLength;
+            ShowFirst = showFirst;
+            ShowLast = showLast;
+            Text = text;
+            Mask = mask ?? "*";
+        }
+    }
+}
